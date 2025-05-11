29aa2283d48dcd545fd8656840df21d7215310e7
“[Noor <nourmamoun@gmail.com>
2025-05-11 14:30:58 +0300

    adding userID inside posts schema

--- db/schema.rb
+++ db/schema.rb
@@ -10,12 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.0].define(version: 2025_05_11_110006) do
+ActiveRecord::Schema[8.0].define(version: 2025_05_11_112804) do
   create_table "posts", force: :cascade do |t|
     t.string "title"
     t.text "content"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "user_id", null: false
+    t.index ["user_id"], name: "index_posts_on_user_id"
   end
 
   create_table "users", force: :cascade do |t|
@@ -26,4 +28,6 @@ ActiveRecord::Schema[8.0].define(version: 2025_05_11_110006) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
+
+  add_foreign_key "posts", "users"
 end
