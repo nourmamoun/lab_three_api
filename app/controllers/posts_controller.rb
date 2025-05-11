10178f708a02d7c4d3484c93a09bacc6362f1872
“[Noor <nourmamoun@gmail.com>
2025-05-11 14:58:32 +0300

    editing post

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,4 +1,5 @@
 class PostsController < ApplicationController
+  skip_before_action :verify_authenticity_token, only: [:create]
   before_action :set_post, only: %i[ show edit update destroy ]
 
   # GET /posts or /posts.json
